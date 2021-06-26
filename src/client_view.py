224a9a465f80c65cc91d91877abd89716eefae9b
bellmatthewf <41313531+bellmatthewf@users.noreply.github.com>
2021-06-26 19:47:55 -0400

    Format with black

--- src/client_view.py
+++ src/client_view.py
@@ -1,103 +1,155 @@
 #!/usr/bin/env python3
 
-#Author: Matthew Bell - 2018
+# Author: Matthew Bell - 2018
 
 """GUI for the Pet Prints client"""
 
-import tkinter as tk
-from tkinter import messagebox
-from tkinter import font as tkfont
-from functools import partial
 import threading
+import tkinter as tk
+from tkinter import font as tkfont
 
 
 class Main_View:
+    def __init__(self, master, controller):
+        self.frame_main = tk.Frame(master)
+        self.frame_main.grid(row=0, column=0, sticky="NSEW")
 
-	def __init__(self, master, controller):
-		self.frame_main = tk.Frame(master)
-		self.frame_main.grid(row=0, column=0, sticky="NSEW")
+        self.font_title = tkfont.Font(family="Helvetica", size=24, weight="bold")
+        self.font_buttonH1 = tkfont.Font(family="Helvetica", size=14, weight="bold")
+        self.font_buttonH2 = tkfont.Font(family="Helvetica", size=12, weight="bold")
+        self.font_unimportant = tkfont.Font(family="Helvetica", size=12)
 
-		self.font_title = tkfont.Font(family="Helvetica", size=24, weight="bold")
-		self.font_buttonH1 = tkfont.Font(family="Helvetica", size=14, weight="bold")
-		self.font_buttonH2 = tkfont.Font(family="Helvetica", size=12, weight="bold")
-		self.font_unimportant = tkfont.Font(family="Helvetica", size=12)
+        self.label_title = tk.Label(
+            self.frame_main, text="Welcome to Pet Prints", font=self.font_title
+        )
+        self.label_title.place(anchor="center", relx=0.5, rely=0.15)
 
-		self.label_title = tk.Label(self.frame_main, text="Welcome to Pet Prints",
-		font=self.font_title)
-		self.label_title.place(anchor="center", relx=.5, rely=.15)
+        self.frame_take_photos = tk.Frame(self.frame_main)
+        self.frame_take_photos.place(
+            relx=0.1, rely=0.4, anchor="w", width=384, height=75
+        )
+        self.frame_take_photos.rowconfigure(0, weight=1)
+        self.frame_take_photos.columnconfigure(0, weight=1)
+        self.button_take_photos = tk.Button(
+            self.frame_take_photos,
+            text="Take Photos",
+            bg="powder blue",
+            bd=8,
+            font=self.font_buttonH1,
+            command=lambda: threading.Thread(
+                target=controller.main_take_photos
+            ).start(),
+        )
+        self.button_take_photos.grid(sticky="NSEW")
 
-		self.frame_take_photos = tk.Frame(self.frame_main)
-		self.frame_take_photos.place(relx=.1, rely=.4, anchor="w", width=384, height=75)
-		self.frame_take_photos.rowconfigure(0, weight=1)
-		self.frame_take_photos.columnconfigure(0, weight=1)
-		self.button_take_photos = tk.Button(self.frame_take_photos, text="Take Photos",
-		bg="powder blue", bd=8, font=self.font_buttonH1, command=lambda: threading.Thread(target=controller.main_take_photos).start())
-		self.button_take_photos.grid(sticky="NSEW")
+        self.frame_help = tk.Frame(self.frame_main)
+        self.frame_help.place(relx=0.1, rely=0.7, anchor="w", width=175, height=75)
+        self.frame_help.rowconfigure(0, weight=1)
+        self.frame_help.columnconfigure(0, weight=1)
+        self.button_help = tk.Button(
+            self.frame_help,
+            text="Help",
+            bg="powder blue",
+            bd=8,
+            font=self.font_buttonH1,
+            command=lambda: controller.messagebox_info(
+                "Help", "Call (416) 801-8237 for assistance!"
+            ),
+        )
+        self.button_help.grid(sticky="NSEW")
 
-		self.frame_help = tk.Frame(self.frame_main)
-		self.frame_help.place(relx=.1, rely=.7, anchor="w", width=175, height=75)
-		self.frame_help.rowconfigure(0, weight=1)
-		self.frame_help.columnconfigure(0, weight=1)
-		self.button_help = tk.Button(self.frame_help, text="Help", bg="powder blue", bd=8, 
-		font=self.font_buttonH1, command=lambda: controller.messagebox_info("Help", "Call (416) 801-8237 for assistance!"))
-		self.button_help.grid(sticky="NSEW")
+        self.frame_shutdown = tk.Frame(self.frame_main)
+        self.frame_shutdown.place(relx=0.9, rely=0.7, anchor="e", width=175, height=75)
+        self.frame_shutdown.rowconfigure(0, weight=1)
+        self.frame_shutdown.columnconfigure(0, weight=1)
+        self.button_shutdown = tk.Button(
+            self.frame_shutdown,
+            text="Shutdown...",
+            bg="powder blue",
+            bd=8,
+            font=self.font_buttonH1,
+            command=lambda: controller.request_shutdown(),
+        )
+        self.button_shutdown.grid(sticky="NSEW")
 
-		self.frame_shutdown = tk.Frame(self.frame_main)
-		self.frame_shutdown.place(relx=.9, rely=.7, anchor="e", width=175, height=75)
-		self.frame_shutdown.rowconfigure(0, weight=1)
-		self.frame_shutdown.columnconfigure(0, weight=1)
-		self.button_shutdown = tk.Button(self.frame_shutdown, text="Shutdown...", 
-		bg="powder blue", bd=8, font=self.font_buttonH1, command=lambda: controller.request_shutdown())
-		self.button_shutdown.grid(sticky="NSEW")
-
-		#Secret quit click main frame the press "Q" to quit program
-		self.frame_main.bind("<Button-1>", lambda x: self.frame_main.focus_set())
-		self.frame_main.bind("q", lambda x: controller.secret_quit())
+        # Secret quit click main frame the press "Q" to quit program
+        self.frame_main.bind("<Button-1>", lambda x: self.frame_main.focus_set())
+        self.frame_main.bind("q", lambda x: controller.secret_quit())
 
 
 class Review_View:
+    def __init__(self, master, controller):
+        self.frame_review = tk.Frame(master)
+        self.frame_review.grid(row=0, column=0, sticky="NSEW")
 
-	def __init__(self, master, controller):
-		self.frame_review = tk.Frame(master)
-		self.frame_review.grid(row=0, column=0, sticky="NSEW")
+        self.font_title = tkfont.Font(family="Helvetica", size=24, weight="bold")
+        self.font_buttonH1 = tkfont.Font(family="Helvetica", size=14, weight="bold")
+        self.font_buttonH2 = tkfont.Font(family="Helvetica", size=12, weight="bold")
+        self.font_unimportant = tkfont.Font(family="Helvetica", size=12)
 
-		self.font_title = tkfont.Font(family="Helvetica", size=24, weight="bold")
-		self.font_buttonH1 = tkfont.Font(family="Helvetica", size=14, weight="bold")
-		self.font_buttonH2 = tkfont.Font(family="Helvetica", size=12, weight="bold")
-		self.font_unimportant = tkfont.Font(family="Helvetica", size=12)
+        self.label_Header = tk.Label(
+            self.frame_review, text="Review Images", font=self.font_title
+        )
+        self.label_Header.place(anchor="center", relx=0.5, rely=0.06)
 
-		self.label_Header = tk.Label(self.frame_review, text="Review Images", 
-		font=self.font_title)
-		self.label_Header.place(anchor="center", relx=.5, rely=.06)
+        self.frame_left = tk.Frame(self.frame_review)
+        self.frame_left.place(relx=0.1, rely=0.5, anchor="center", width=40, height=100)
+        self.frame_left.rowconfigure(0, weight=1)
+        self.frame_left.columnconfigure(0, weight=1)
+        self.button_left = tk.Button(
+            self.frame_left,
+            text="L",
+            bd=8,
+            bg="powder blue",
+            font=self.font_buttonH2,
+            command=lambda: controller.scroll_photos("left"),
+        )
+        self.button_left.grid(sticky="NSEW")
 
-		self.frame_left = tk.Frame(self.frame_review)
-		self.frame_left.place(relx=.1, rely=.5, anchor="center", width=40, height=100)
-		self.frame_left.rowconfigure(0, weight=1)
-		self.frame_left.columnconfigure(0, weight=1)
-		self.button_left = tk.Button(self.frame_left, text="L", bd=8, bg="powder blue", 
-		font=self.font_buttonH2, command=lambda: controller.scroll_photos("left"))
-		self.button_left.grid(sticky="NSEW")
+        self.frame_right = tk.Frame(self.frame_review)
+        self.frame_right.place(
+            relx=0.9, rely=0.5, anchor="center", width=40, height=100
+        )
+        self.frame_right.rowconfigure(0, weight=1)
+        self.frame_right.columnconfigure(0, weight=1)
+        self.button_right = tk.Button(
+            self.frame_right,
+            text="R",
+            bd=8,
+            bg="powder blue",
+            font=self.font_buttonH2,
+            command=lambda: controller.scroll_photos("right"),
+        )
+        self.button_right.grid(sticky="NSEW")
 
-		self.frame_right = tk.Frame(self.frame_review)
-		self.frame_right.place(relx=.9, rely=.5, anchor="center", width=40, height=100)
-		self.frame_right.rowconfigure(0, weight=1)
-		self.frame_right.columnconfigure(0, weight=1)
-		self.button_right = tk.Button(self.frame_right, text="R", bd=8, bg="powder blue", 
-		font=self.font_buttonH2, command=lambda: controller.scroll_photos("right"))
-		self.button_right.grid(sticky="NSEW")
+        self.frame_upload = tk.Frame(self.frame_review)
+        self.frame_upload.place(
+            anchor="center", relx=0.31, rely=0.91, width=170, height=45
+        )
+        self.frame_upload.rowconfigure(0, weight=1)
+        self.frame_upload.columnconfigure(0, weight=1)
+        self.button_upload = tk.Button(
+            self.frame_upload,
+            text="Upload Photos",
+            bd=8,
+            bg="powder blue",
+            font=self.font_buttonH2,
+            command=lambda: threading.Thread(target=controller.main_upload).start(),
+        )
+        self.button_upload.grid(sticky="NSEW")
 
-		self.frame_upload = tk.Frame(self.frame_review)
-		self.frame_upload.place(anchor="center", relx=.31, rely=.91, width=170, height=45)
-		self.frame_upload.rowconfigure(0, weight=1)
-		self.frame_upload.columnconfigure(0, weight=1)
-		self.button_upload = tk.Button(self.frame_upload, text="Upload Photos", bd=8, 
-		bg="powder blue", font=self.font_buttonH2, command=lambda: threading.Thread(target=controller.main_upload).start())
-		self.button_upload.grid(sticky="NSEW")
-
-		self.frame_retake = tk.Frame(self.frame_review)
-		self.frame_retake.place(anchor="center", relx=.69, rely=.91, width=170, height=45)
-		self.frame_retake.rowconfigure(0, weight=1)
-		self.frame_retake.columnconfigure(0, weight=1)
-		self.button_retake_photos = tk.Button(self.frame_retake, text="Retake Photos", 
-		bd=8, bg="powder blue", font=self.font_buttonH2, command=lambda: controller.confirm_retake())
-		self.button_retake_photos.grid(sticky="NSEW")
+        self.frame_retake = tk.Frame(self.frame_review)
+        self.frame_retake.place(
+            anchor="center", relx=0.69, rely=0.91, width=170, height=45
+        )
+        self.frame_retake.rowconfigure(0, weight=1)
+        self.frame_retake.columnconfigure(0, weight=1)
+        self.button_retake_photos = tk.Button(
+            self.frame_retake,
+            text="Retake Photos",
+            bd=8,
+            bg="powder blue",
+            font=self.font_buttonH2,
+            command=lambda: controller.confirm_retake(),
+        )
+        self.button_retake_photos.grid(sticky="NSEW")
--- src/server.py
+++ src/server.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
-#Author: Matthew Bell - 2018
+# Author: Matthew Bell - 2018
 
-"""Server script for the Pet Prints client. 
+"""Server script for the Pet Prints client.
 Takes a command from the client and performs requisite actions"""
 
-import custom_socket, picamera
+import traceback
+from os import remove
 from subprocess import Popen
 from threading import Thread
-from os import remove
 from time import sleep
-import traceback
+
+import picamera
+
+import custom_socket
 
 show_tracebacks = False
 
@@ -19,88 +22,92 @@ src_port = 2222
 dest_host = "192.168.3.2"
 dest_port = 2222
 
+
 class Server:
-	def __init__(self, src_host, src_port, dest_host, dest_port):
-		self.src_host = src_host
-		self.src_port = src_port
-		self.dest_host = dest_host
-		self.dest_port = dest_port
-		self.sockets = custom_socket.Sockets(self.src_host, self.src_port, 
-		self.dest_host, self.dest_port)
-		Thread(target=self.ping).start()
-		self.decode_calls()
+    def __init__(self, src_host, src_port, dest_host, dest_port):
+        self.src_host = src_host
+        self.src_port = src_port
+        self.dest_host = dest_host
+        self.dest_port = dest_port
+        self.sockets = custom_socket.Sockets(
+            self.src_host, self.src_port, self.dest_host, self.dest_port
+        )
+        Thread(target=self.ping).start()
+        self.decode_calls()
 
-	def ping(self):
-		self.sockets.create_socket(tcp = True)
-		counter = 0
-		while counter < 5:
-			try:
-				self.sockets.send_tcp_data(b"ping", 1024, True)
-				break
-			except Exception as e:
-				print("Failed to ping client")
-				if show_tracebacks:
-					traceback.print_exc()
-				counter += 1
-				sleep(5)
-		self.sockets.close_socket(tcp = True)
+    def ping(self):
+        self.sockets.create_socket(tcp=True)
+        counter = 0
+        while counter < 5:
+            try:
+                self.sockets.send_tcp_data(b"ping", 1024, True)
+                break
+            except Exception as e:
+                print("Failed to ping client")
+                if show_tracebacks:
+                    traceback.print_exc()
+                counter += 1
+                sleep(5)
+        self.sockets.close_socket(tcp=True)
 
-	def decode_calls(self):
-		self.sockets.create_socket(udp = True)
-		while True:
-			print("Server listening")
-			data = self.sockets.recv_udp_data(1024)
-			print("Received data")
-			Thread(target=self.interpret_command, args=(data,)).start()
+    def decode_calls(self):
+        self.sockets.create_socket(udp=True)
+        while True:
+            print("Server listening")
+            data = self.sockets.recv_udp_data(1024)
+            print("Received data")
+            Thread(target=self.interpret_command, args=(data,)).start()
 
-	def interpret_command(self, data):
-		command = data.decode("utf-8")
-		if command == "take_photo":
-			print("Taking photo")
-			self.take_photo()
-		elif command == "remit_photo":
-			print("Remitting photo to client")
-			self.remit_photo()
-		elif command == "shutdown":
-			print("Shutting down")
-			self.shutdown()
+    def interpret_command(self, data):
+        command = data.decode("utf-8")
+        if command == "take_photo":
+            print("Taking photo")
+            self.take_photo()
+        elif command == "remit_photo":
+            print("Remitting photo to client")
+            self.remit_photo()
+        elif command == "shutdown":
+            print("Shutting down")
+            self.shutdown()
 
-	def take_photo(self):
-		camera = picamera.PiCamera()
-		try:
-			with camera:
-				camera.resolution = (1280, 720)
-				camera.capture("/home/pi/Pictures/img.jpg")
-		except Exception as e:
-			if show_tracebacks:
-				traceback.print_exc()
-		else:
-			self.sockets.send_udp_data(b"Taken")
+    def take_photo(self):
+        camera = picamera.PiCamera()
+        try:
+            with camera:
+                camera.resolution = (1280, 720)
+                camera.capture("/home/pi/Pictures/img.jpg")
+        except Exception as e:
+            if show_tracebacks:
+                traceback.print_exc()
+        else:
+            self.sockets.send_udp_data(b"Taken")
 
-	def remit_photo(self):
-		file_loc = "/home/pi/Pictures/img.jpg"
-		self.sockets.create_socket(tcp = True)
-		try:
-			f = open(file_loc, "rb")
-			data = f.read()
-		except Exception as e:
-			traceback.print_exc()
-			data = None
-		if data:
-			try:
-				self.sockets.send_tcp_data(data, 8192, True)
-			except Exception as e:
-				if show_tracebacks:
-					traceback.print_exc()
-		self.sockets.close_socket(tcp = True)
-		remove(file_loc)
+    def remit_photo(self):
+        file_loc = "/home/pi/Pictures/img.jpg"
+        self.sockets.create_socket(tcp=True)
+        try:
+            f = open(file_loc, "rb")
+            data = f.read()
+        except Exception as e:
+            traceback.print_exc()
+            data = None
+        if data:
+            try:
+                self.sockets.send_tcp_data(data, 8192, True)
+            except Exception as e:
+                if show_tracebacks:
+                    traceback.print_exc()
+        self.sockets.close_socket(tcp=True)
+        remove(file_loc)
 
-	def shutdown(self):
-		self.sockets.close_socket(udp = True)
-		Popen(["sudo", "poweroff"])
+    def shutdown(self):
+        self.sockets.close_socket(udp=True)
+        Popen(["sudo", "poweroff"])
+
 
 def main(src_host, src_port, dest_host, dest_port):
-	Server(src_host, src_port, dest_host, dest_port)
+    Server(src_host, src_port, dest_host, dest_port)
+
 
 if __name__ == "__main__":
-	main(src_host, src_port, dest_host, dest_port)
+    main(src_host, src_port, dest_host, dest_port)
